9d1d05180715e29b387794152888fb4c7026370f
Francisco <victorifrancis2009@gmail.com>
2020-11-24 00:30:37 -0300

    cpp update

--- C/cpp/LeL.cpp
+++ C/cpp/LeL.cpp
@@ -17,33 +17,41 @@ class Item{
 		Item(int precio, string nombre, int danio, string tipo_danio){
 			this->precio = precio;
 			this->nombre = nombre;
-			this->danio = dnaio;
+			this->danio = danio;
 			this->tipo_danio = tipo_danio; 
 		}
 
-		string getNombre(){
+		string getNombre() const{
 			return this->nombre;
 		}
 
-		int getPrecio(){
+		int getPrecio() const{
 			return this->precio;
+		}
+
+		int getDanio() const{
+			return this->danio;
+		}
+
+		string getTipoDanio() const{
+			return this->tipo_danio;
+		}
+
+		void show() const{
+			cout << this->getNombre() << endl
+				 << this->getPrecio() << endl
+				 << this->getDanio() << endl
+				 << this->getTipoDanio() << endl << endl;
 		}
 
 	
 };
 
-class Tienda{
-	private:
-		vector<vector<int>> paginas;
-	public:
-		Tienda();
-	
-};
-
+//clase para la cola de prioridad que ordena de menor a mayor
 class myComparison
 {
 public:
-    bool operator() (const Item& it1, const Item& it2)
+    bool operator() (Item it1, Item it2)
     {
         return it1.getPrecio() > it2.getPrecio();
     }
@@ -51,33 +59,31 @@ public:
 
 class Pagina{
 	private:
-		string tipo_danio;
 		priority_queue<Item, vector<Item>, myComparison> items;
-	public:
-		Pagina(string tipo_danio){
-			this->tipo_danio = tipo_danio;
-		}
 
-		priority_queue<Item> getItems(){
-			return items;
+	public:
+
+		priority_queue<Item, vector<Item>, myComparison> getItems(){
+			return this->items;
 		}
 
 		void addItem(Item item){
 			this->items.push(item);
 		}
 
-		void despliegue(priority_queue<Item> pq){
+		void despliegue(){
+			priority_queue<Item, vector<Item>, myComparison> pq = this->getItems();
 			while(!pq.empty()){
-				cout << " " << pq.front();
+				pq.top().show();
 				pq.pop();
 			}
 			cout << endl;
 		}
 
-		bool removeItem(string nombreItem){
+		void removeItem(string nombreItem){
 			vector<Item> aux;
 			while(!this->items.empty()){
-				if (this->items.top()->getNombre() != nombreItem){
+				if (this->items.top().getNombre() != nombreItem){
 					aux.push_back(this->items.top());
 					this->items.pop();
 				}
@@ -90,8 +96,8 @@ class Pagina{
 		void comproTodo(int valor){
 			vector<string> nombres;
 			int suma=0;
-			while(TRUE){
-				if (suma < valor){
+			while(true){
+				if (suma + this->items.top().getPrecio() < valor){
 					suma += this->items.top().getPrecio();
 					nombres.push_back(this->items.top().getNombre());
 					this->items.pop();
@@ -100,8 +106,71 @@ class Pagina{
 				}
 				
 			}
-			//imprimir
+			cout << "con " << valor << " puede comprar";
+			for (int i = 0; i < nombres.size(); ++i){
+				cout<< " " << nombres[i];
+			}
+			cout << " que suman " << suma << endl;
+
 			
 		}
 
 };
+
+class Tienda{
+	private:
+		vector<vector<Pagina>> paginas{{},{},{},{}}; //son 4 tipo de daño
+	public:
+		
+		void addPagina(Pagina P, int i){
+			this->paginas[i-1].push_back(P);
+		}
+
+		void menu(){
+			int cat;
+			cout << "Indique la página a la que quiere acceder" << endl
+				 << "1 Defensa" << endl
+				 << "2 Mágico" << endl
+				 << "3 Físico" << endl
+				 << "4 Crítico" << endl;
+			cin >> cat;
+			for (int i = 0; i < paginas[cat-1].size(); ++i){
+				paginas[cat-1][i].despliegue();
+			}
+			
+ 		}
+
+ 		vector<vector<Pagina>> getPaginas(){
+ 			return this->paginas;
+ 		}
+	
+};
+
+class PaginaFisico: public Pagina{};
+
+class PaginaDefensa: public Pagina{};
+
+class PaginaMAgico: public Pagina{};
+
+class PaginaCritico: public Pagina{};
+
+
+int main()
+{
+	Item espada = Item(700, "espada", 100, "Físico");
+	Item hacha = Item(500, "hacha", 300, "Físico");
+	Item guillotina = Item(1000, "guillotina", 500, "Físico");
+	Item posion = Item(400, "posion", 100, "Mágico");
+
+	PaginaFisico fisico;
+	fisico.addItem(espada);
+	fisico.addItem(hacha);
+	fisico.addItem(guillotina);
+
+	Tienda mitienda;
+	mitienda.addPagina(fisico,3);
+	mitienda.menu();
+	mitienda.getPaginas()[2][0].comproTodo(2000);
+
+	return 0;
+}
